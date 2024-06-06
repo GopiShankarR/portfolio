48b3136b8450ed4afeeef26008e09f8d39b21af0
Gopi Shankar Ravady <gopi2605@gmail.com>
2024-06-06 15:19:54 -0500

    removed routes

--- src/App.js
+++ src/App.js
@@ -13,16 +13,6 @@ import About from './components/About';
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/portfolio" element={<Header />} />
-        <Route path="/#about" element={<About />} />
-        <Route path="/#skills" element={<Skills />} />
-        <Route path="/#education" element={<Education />} />
-        <Route path="/#experience" element={<Experience />} />
-        <Route path="/#projects" element={<Projects />} />
-        <Route path="/#contact" element={<Contact />} />
-
-      </Routes>
       <Header />
       <Hero />
       <About />
