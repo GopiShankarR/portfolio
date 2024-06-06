70aeb2c8e5bad0a1abcbffa05e5c5f72c7954211
Gopi Shankar Ravady <gopi2605@gmail.com>
2024-06-06 15:05:09 -0500

    added router via react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Header from './components/Header';
 import Contact from './components/Contact';
+import { Routes, Route} from "react-router-dom";
 import Hero from './components/Hero';
 import Projects from './components/Projects';
 import Footer from './components/Footer';
@@ -12,6 +13,16 @@ import About from './components/About';
 function App() {
   return (
     <>
+      <Routes>
+        <Route path="/portfolio" element={<Header />} />
+        <Route path="/#about" element={<About />} />
+        <Route path="/#skills" element={<Skills />} />
+        <Route path="/#education" element={<Education />} />
+        <Route path="/#experience" element={<Experience />} />
+        <Route path="/#projects" element={<Projects />} />
+        <Route path="/#contact" element={<Contact />} />
+
+      </Routes>
       <Header />
       <Hero />
       <About />
