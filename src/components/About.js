b2e609511fd6b4016fdd44b457fefdeb7e6b6e07
Gopi Shankar Ravady <gopi2605@gmail.com>
2024-06-06 18:15:01 -0500

    grammar changes for about me

--- src/components/About.js
+++ src/components/About.js
@@ -7,14 +7,15 @@ const About= () => {
         <div className="about-info">
           <h2 className="text-4xl font-bold mb-5 border-b-[5px] w-[180px] border-indigo-600 pb-1">About Me</h2>
           <p className="font-serif pb-5">
-            Hi, My Name Is Gopi Shankar Ravady. I am a
-            Full Stack Developer with a zest to learn new tech and get updated with technology every day.
+            Hi, My Name Is Gopi Shankar Ravady. I am a Full Stack Developer with a zest for learning new tech and getting updated with technology every day.
           </p>
           <p className="font-serif pb-5">
-            I am proficient in Frontend skills like Vue.js, Flutter, HTML, CSS, JavaScript, Tailwind CSS and currently I'm learning React.js.
+            I am proficient in Frontend skills like Vue.js, Flutter, HTML, CSS, JavaScript, and Tailwind CSS, and currently, I'm learning React.js.
           </p>
 
-          <p className="font-serif pb-2">In the Backend, I have learned Node.js, Nest.js, Ruby, C++, MySQL, and MongoDB</p>
+          <p className="font-serif pb-2">
+            In the Backend, I have learned Node.js, Nest.js, Ruby, C++, MySQL, and MongoDB
+          </p>
         </div>
       </div>
 
